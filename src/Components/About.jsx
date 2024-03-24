29b0c69017a30bdd7890c1f57f53fad0a6625374
Hsiao Han Huang <126027281+SylviaHuang960603@users.noreply.github.com>
2024-03-24 02:40:09 +0000

    modified:   src/Components/About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,7 +16,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/Back.jpg";
+import image from "<div styleName="" />
+<images />
+<Back></Back>.jpg";
 
 const imageAltText = "grey background";
 
